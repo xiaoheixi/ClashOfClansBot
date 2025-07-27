89fbff8f69d1054e45c6556b2670a9f70362e81d
xioaheixi <justinzhao893@gmail.com>
2025-07-27 14:58:35 +1000

    Fixed bug, not releasing down key.

--- Masud.py
+++ Masud.py
@@ -8,6 +8,7 @@ while True:
     time.sleep(5)  # Wait for 5 second
     for i in range(5):
         pyautogui.keyDown('down') # Zoom out
+    pyautogui.keyUp('down')  # Release the down key
     pyautogui.click(1408, 1359)  # Dragon!
     for i in range(17): 
         pyautogui.click(2403, 1063)  # Deploy Dragons at right corner!
@@ -23,7 +24,7 @@ while True:
     pyautogui.click(1714, 1365)  # Activate Hero 2 ability!
     pyautogui.click(1796, 1357)  # Activate Hero 3 ability!
     pyautogui.click(1896, 1363)  # Activate Hero 4 ability!
-    time.sleep(95)  # Wait for 95 seconds
+    time.sleep(95)  # Wait for 90 seconds
     pyautogui.click(1371, 1263)  # Surrender
     time.sleep(1)  # Wait for 1 second
     pyautogui.click(2044, 1190)  # Okay
