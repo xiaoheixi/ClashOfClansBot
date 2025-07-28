854751f236dfc98421af9a6ef5be420f5df8fdae
Justin <justinzhao893@gmail.com>
2025-07-29 08:50:09 +1000

    Coordinates updated, Address Errors left.

--- UniversalMac.py
+++ UniversalMac.py
@@ -11,27 +11,27 @@ while True:
     for i in range(5):
         pyautogui.keyDown('down') # Zoom out
     pyautogui.keyUp('down')  # Release the down key
-    pyautogui.click(1093, 1578)  # Dragon!
+    pyautogui.click(1315, 1575)  # Dragon!
     for i in range(17): 
-        pyautogui.click(2347, 1207)  # Deploy Dragons at right corner!
-    pyautogui.click(1364, 1580)  # Hero 1 Selection!
-    pyautogui.click(2347, 1207)  # Deploy Hero 1 at right corner!
-    pyautogui.click(1487, 1580)  # Hero 2 Selection!
-    pyautogui.click(2347, 1207)  # Deploy Hero 2 at right corner!
-    pyautogui.click(1608, 1580)  # Hero 3 Selection!
-    pyautogui.click(2347, 1207)  # Deploy Hero 3 at right corner!
-    pyautogui.click(1727, 1580)  # Hero 4 Selection!
-    pyautogui.click(2347, 1207)  # Deploy Hero 4 at right corner!
-    pyautogui.click(1364, 1580)  # Activate Hero 1 ability!
-    pyautogui.click(1487, 1580)  # Activate Hero 2 ability!
-    pyautogui.click(1608, 1580)  # Activate Hero 3 ability!
-    pyautogui.click(1727, 1580)  # Activate Hero 4 ability!
+        pyautogui.click(2366, 1263)  # Deploy Dragons at right corner!
+    pyautogui.click(1544, 1575)  # Hero 1 Selection!
+    pyautogui.click(2366, 1263)  # Deploy Hero 1 at right corner!
+    pyautogui.click(1656, 1575)  # Hero 2 Selection!
+    pyautogui.click(2366, 1263)  # Deploy Hero 2 at right corner!
+    pyautogui.click(1748, 1575)  # Hero 3 Selection!
+    pyautogui.click(2366, 1263)  # Deploy Hero 3 at right corner!
+    pyautogui.click(1845, 1575)  # Hero 4 Selection!
+    pyautogui.click(2366, 1263)  # Deploy Hero 4 at right corner!
+    pyautogui.click(1544, 1575)  # Activate Hero 1 ability!
+    pyautogui.click(1656, 1575)  # Activate Hero 2 ability!
+    pyautogui.click(1748, 1575)  # Activate Hero 3 ability!
+    pyautogui.click(1845, 1575)  # Activate Hero 4 ability!
     time.sleep(87)  # Wait for 87 seconds
-    pyautogui.click(1043, 1438)  # Surrender
+    pyautogui.click(1277, 1461)  # Surrender
     time.sleep(1)  # Wait for 1 second
-    pyautogui.click(1905, 1340)  # Okay
+    pyautogui.click(1998, 1383)  # Okay
     time.sleep(2)  # Wait for 2 second
-    pyautogui.click(1719, 1531)  # Return Home!
+    pyautogui.click(1850, 1547)  # Return Home!
     time.sleep(5)  # Wait for 5 seconds
     pyautogui.click(1652, 1152)  # Address Errors!
     time.sleep(9)  # Wait for 9 seconds
