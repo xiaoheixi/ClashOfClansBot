a4e4cfd97aae2d886e9ebd97378a5b05a7ac9276
xioaheixi <justinzhao893@gmail.com>
2025-08-01 19:14:58 +1000

    Updated UniversalWindows for latest update.

--- UniversalWindows.py
+++ UniversalWindows.py
@@ -11,21 +11,24 @@ while True:
     for i in range(5):
         pyautogui.keyDown('down') # Zoom out
     pyautogui.keyUp('down')  # Release the down key
-    pyautogui.click(1408, 1359)  # Dragon!
+    pyautogui.click(1513, 1365)  # Dragon!
     for i in range(17): 
         pyautogui.click(2403, 1063)  # Deploy Dragons at right corner!
-    pyautogui.click(1619, 1363)  # Hero 1 Selection!
+    # pyautogui.click(1619, 1363)  # Hero 1 Selection!
+    # pyautogui.click(2403, 1063)  # Deploy Hero 1 at right corner!
+    pyautogui.click(1714, 1365)  # Hero 1 Selection!
     pyautogui.click(2403, 1063)  # Deploy Hero 1 at right corner!
-    pyautogui.click(1714, 1365)  # Hero 2 Selection!
+    pyautogui.click(1796, 1357)  # Hero 2 Selection!
     pyautogui.click(2403, 1063)  # Deploy Hero 2 at right corner!
-    pyautogui.click(1796, 1357)  # Hero 3 Selection!
+    pyautogui.click(1896, 1363)  # Hero 3 Selection!
     pyautogui.click(2403, 1063)  # Deploy Hero 3 at right corner!
-    pyautogui.click(1896, 1363)  # Hero 4 Selection!
+    pyautogui.click(1986, 1363)  # Hero 4 Selection!
     pyautogui.click(2403, 1063)  # Deploy Hero 4 at right corner!
-    pyautogui.click(1619, 1363)  # Activate Hero 1 ability!
-    pyautogui.click(1714, 1365)  # Activate Hero 2 ability!
-    pyautogui.click(1796, 1357)  # Activate Hero 3 ability!
-    pyautogui.click(1896, 1363)  # Activate Hero 4 ability!
+    # pyautogui.click(1619, 1363)  # Activate Hero 1 ability!
+    pyautogui.click(1714, 1365)  # Activate Hero 1 ability!
+    pyautogui.click(1796, 1357)  # Activate Hero 2 ability!
+    pyautogui.click(1896, 1363)  # Activate Hero 3 ability!
+    pyautogui.click(1986, 1363)  # Activate Hero 4 ability!
     time.sleep(87)  # Wait for 87 seconds
     pyautogui.click(1371, 1263)  # Surrender
     time.sleep(1)  # Wait for 1 second
